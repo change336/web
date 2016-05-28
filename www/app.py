e04432acae2b4a1637ff086367235790f82d721c
michaelmakun <michaelmakun@aliyun.com>
2016-05-28 17:39:55 +0800

    acturally this is day05

--- www/app.py
+++ www/app.py
@@ -48,16 +48,117 @@ from datetime import datetime
 
 from aiohttp import web
 #aiohttp是基于asyncio实现的HTTP框架,同时支持HTTP客户端和服务端,导入web函数
+from jinja2 import Environment,FileSystemLoader
+import orm
+from coroweb import add_routes,add_static
 
-def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
+def init_jinja(app,**kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape',True),
+		block_start_string = kw.get('block_start_string','{%'),
+		block_end_string = kw.get('block_end_string','%}'),
+		variable_start_string = kw.get('variable_start_string','{{'),
+		variable_end_string = kw.get('variable_end_string','}}'),
+		auto_reload = kw.get('auto_reload',True)
+	)
+	path = kw.get('path',None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+		logging.info('set jinja2 template path:%s' % path)
+		env = Environment(loader=FileSystemloader(path),**options)
+		if filters is not None:
+			for name,f in filters.items():
+				env.filters[name] = f
+		app['__templating__'] = env
+		
+async def logger_factory(app,handler):
+	async def logger(request):
+		logging.info('Request: %s %s' % (request.method,request.path))
+		#await asyncio.sleep(0.3)
+		return (await handler(request))
+	return logger
+	
+async def data_factory(app,handler):
+	async def parse_data(request):
+		if request.method == 'POST':
+			if request.content_type.startswith('application/json'):
+				request.__data__=await request.json()
+				logging.info('request json:%s' % str(request.__data__))
+			elif request.content_type.startwith('application/x-www-form-urlencoded'):
+				request.__data__=await request.post()
+				logging.info('request form:%s' % str(request.__data__))
+		return (await handle(request))
+	return parse_data
+	
+async def response_factory(app,handler):
+	async def response(request):
+		logging.info('Response handle...')
+		r = await handle(request)
+		if isinstance(r,web.StreamResponse):
+			return r
+		if isinstance(r,bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r,str):
+			if r.startwith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if isinstance(r,dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r,ensure_ascii=False,default=lambda o:o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset = utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r,int) and r>=100 and r <600:
+			return web.Response(r)
+		if isinstance(r,tuple) and len(r) ==2:
+			t,m = r
+			if isinstance(t,int) and t>=100 and t<600:
+				return web.Response(t,str(m))
+		#default:
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
+	
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta<60:
+		return u'1分钟前'
+	if delta<3600:
+		return u'%s分钟前' % (delta//60)
+	if delta<86400:
+		return u'%小时前' % (delta//3600)
+	if delta<608400:
+		return u'天前' % (delta//86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%月%日' % (dt.year,dt.month,dt.day)
+	
+
+
+#def index(request):
+	#return web.Response(body=b'<h1>Awesome</h1>')
 #定义处理http请求的方法，等号后面的是响应返回的内容
 
 async def init(loop):
 #定义一个初始函数，函数作用是返回一个服务器对象
-	app = web.Application(loop=loop)
+	await orm.creat_pool(loop,host='127.0.0.1',port=3306,user='www',password='www',db='awesome')
+	app = web.Application(loop=loop,middlewares=[logger_factory,response_factory
+	])
+	init_jinja2(app,filters=dict(datetime=datetime_filter))
+	app_routes(app,'handlers')
+	add_static(app)
+
 	#创建一个循环对象是消息循环的web应用对象
-	app.router.add_route('GET','/',index)
+	#app.router.add_route('GET','/',index)
 	#将浏览器通过GET方式传过来的对根目录的请求转发给index函数进行处理
 	srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	#调用子协程，创建一个TCP服务器，绑定到“127.0.0.1：9000”socket,并返回一个服务器对象，用来监听这个端口
